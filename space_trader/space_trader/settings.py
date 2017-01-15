c4470d8b1e4d6085b9b6eba00031a3f026b17845
Marcin Brzescianski <Marcin.brzescianski@gmail.com>
2017-01-15 23:26:52 +0100

    last ver

--- space_trader/space_trader/settings.py
+++ space_trader/space_trader/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'st_app',
+    'bootstrap3'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,14 @@ WSGI_APPLICATION = 'space_trader.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'HOST': '127.0.0.1',
+        'NAME': 'space_trader',
+        'ENGINE': 'mysql.connector.django',
+        'USER': 'root',
+        'PASSWORD': 'coderslab',
+        'OPTIONS': {
+            'autocommit': True,
+        },
     }
 }
 
@@ -111,10 +119,14 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'public')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
--- space_trader/st_app/models.py
+++ space_trader/st_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 # Create your models here.
-
+'''
 class Player(models.Model):
     nick = models.CharField(max_length=12)
     creation_date = models.DateTimeField(default=timezone.now)
@@ -11,8 +11,16 @@ class Player(models.Model):
         return self.nick
 
 class Ship(models.Model):
+    player=models.ForeignKey(Player)
     capacity
     attack
     defence
     speed
+
+class Planet(models.Model):
+    pass
+
+
+class Product(models.Model):
+'''
     
