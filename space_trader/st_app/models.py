88bcad64600baf71188aa54730af574ebf7df92c
Marcin Brzescianski <Marcin.brzescianski@gmail.com>
2017-01-16 19:11:48 +0100

    last ver

--- space_trader/st_app/models.py
+++ space_trader/st_app/models.py
@@ -1,26 +1,32 @@
 from django.db import models
+from django.utils import timezone
 
 
 # Create your models here.
-'''
+
 class Player(models.Model):
     nick = models.CharField(max_length=12)
     creation_date = models.DateTimeField(default=timezone.now)
+    money = models.IntegerField(null=False)
 
     def __str__(self):
         return self.nick
 
-class Ship(models.Model):
-    player=models.ForeignKey(Player)
-    capacity
-    attack
-    defence
-    speed
 
 class Planet(models.Model):
-    pass
+    planet_name = models.CharField(max_length=12)
+
+
+class Ship(models.Model):
+    player = models.ForeignKey(Player)
+    capacity = models.IntegerField(null=False)
+    attack = models.IntegerField(null=False)
+    defence = models.IntegerField(null=False)
+    speed = models.IntegerField(null=False)
+    planet = models.OneToOneField(Planet)
 
 
 class Product(models.Model):
-'''
-    
+    product_name = models.CharField(max_length=12)
+    how_many_space = models.IntegerField(null=False)
+    actual_price = models.IntegerField(null=False)
