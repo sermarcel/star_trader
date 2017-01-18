a1e9d59613a64ef60db600e473a8205e81ba6aac
Marcin Brzescianski <Marcin.brzescianski@gmail.com>
2017-01-18 13:04:35 +0100

    last ver

--- space_trader/st_app/tests.py
+++ space_trader/st_app/tests.py
@@ -1,3 +1,48 @@
 from django.test import TestCase
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import ShipSerializer, PlanetSerializer, PlayerSerializer, ProductSerializer
+from django.views.generic.edit import CreateView, DeleteView, UpdateView, FormView
+from rest_framework import status
+from st_app.models import Ship, Planet, Player, Product
+from django.http import Http404
+from rest_framework import generics
+from random import randint
+from space_trader.settings import os,sys
+
 
 # Create your tests here.
+
+
+
+def env(fun):
+    def _fun_wrap(*args, **kwargs):
+        import os
+        import sys
+
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "%s.settings" % SETTINGS_PATH)
+        sys.path.append(DJANGO_PATH)
+
+        return fun(*args, **kwargs)
+
+    return _fun_wrap
+
+
+@env
+def product_price():
+    planet_count=Planet.objects.count()
+    product_count=Product.objects.count()
+    price_list=[]
+    for pl in range (1, planet_count):
+        for pr in range(1, product_count):
+            pl =randint(1,100)
+            pr =randint(1,100)
+            price_list.append([pl,pr])
+            
+    return price_list
+    # planet=Planet.objects.get(pk=id_planet)
+    # product=Product.objects.get(pk=id_price)
+    #return 'Dla planety {} cena produktu {} wynosi {}'.format(planet, product, price)
+    
+print(product_price())
--- space_trader/st_app/views.py
+++ space_trader/st_app/views.py
@@ -5,9 +5,10 @@ from rest_framework.response import Response
 from .serializers import ShipSerializer, PlanetSerializer, PlayerSerializer, ProductSerializer
 from django.views.generic.edit import CreateView, DeleteView, UpdateView, FormView
 from rest_framework import status
-from .models import Ship, Planet, Player, Product
+from st_app.models import Ship, Planet, Player, Product
 from django.http import Http404
 from rest_framework import generics
+from random import randint
 
 
 
@@ -54,7 +55,36 @@ class PlayerCreate(CreateView):
     template_name='st_app/stage1.html'
     success_url = '/st_app/main.html'
 
-# buy Products
+# Product price on each planet
+
+def product_price():
+    planet_count=Planet.objects.count()
+    product_count=Product.objects.count()
+    price_list=[]
+    
+    print (product_count)
+    print (planet_count)
+    
+    for pl in range (0, planet_count):
+        price_product_list=[]
+        for pr in range(0, product_count):
+            pl =randint(0,100)
+            pr =randint(0,100)
+            price_product_list.append(pl)
+           # price_list[pl].append([pr])
+        price_list.append(price_product_list)
+    print(price_list[1][1])       
+    return price_list
+    # planet=Planet.objects.get(pk=id_planet)
+    # product=Product.objects.get(pk=id_price)
+    #return 'Dla planety {} cena produktu {} wynosi {}'.format(planet, product, price)
+    
+print(product_price())
+
+
+
+
+
 
 
 
